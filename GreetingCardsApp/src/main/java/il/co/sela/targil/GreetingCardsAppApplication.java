30c5f69a99e08b2ef063d8a4f3bcefa7995b48de
issverg <issverg@gmail.com>
2019-07-03 15:30:02 +0300

    first commit

---
+++ GreetingCardsApp/src/main/java/il/co/sela/targil/GreetingCardsAppApplication.java
@@ -0,0 +1,13 @@
+package il.co.sela.targil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingCardsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingCardsAppApplication.class, args);
+	}
+
+}
