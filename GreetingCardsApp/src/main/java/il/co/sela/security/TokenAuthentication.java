4760288d7bec8fec2bf5e403178cd9b6d69a4fad
issverg <issverg@gmail.com>
2019-07-04 12:30:18 +0300

    Implementing security entities for jwt token

---
+++ GreetingCardsApp/src/main/java/il/co/sela/security/TokenAuthentication.java
@@ -0,0 +1,61 @@
+package il.co.sela.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+public class TokenAuthentication implements Authentication {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private boolean isAuthenticated;
+	@Getter @Setter
+	private String token;
+	@Getter @Setter
+	private UserDetails userDetails;
+	@Setter
+	private Collection<? extends GrantedAuthority> auths;
+
+	@Override
+	public String getName() {
+		return userDetails.getUsername();
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return auths;
+	}
+
+	@Override
+	public Object getCredentials() {
+		return null;
+	}
+
+	@Override
+	public Object getDetails() {
+		return userDetails;
+	}
+
+	@Override
+	public Object getPrincipal() {
+		return null;
+	}
+
+	@Override
+	public boolean isAuthenticated() {
+		return isAuthenticated;
+	}
+
+	@Override
+	public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+		this.isAuthenticated = isAuthenticated;
+	}
+}
---
+++ GreetingCardsApp/src/main/java/il/co/sela/security/TokenAuthenticationFilter.java
@@ -0,0 +1,42 @@
+package il.co.sela.security;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class TokenAuthenticationFilter implements Filter {
+	TokenAuthenticationManager manager;
+	
+	public TokenAuthenticationFilter(TokenAuthenticationManager tokenManager) {
+		super();
+		this.manager = tokenManager;
+	}	
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+		HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+		TokenAuthentication tAuthentication = new TokenAuthentication();
+		
+		String token = httpServletRequest.getHeader("token");
+		
+		if (token != null) {
+			tAuthentication.setToken(token);
+		}
+		
+		Authentication authentication = manager.authenticate(tAuthentication);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		
+		chain.doFilter(httpServletRequest, httpServletResponse);
+	}
+}
