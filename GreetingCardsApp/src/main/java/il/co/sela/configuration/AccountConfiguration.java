6e0552fb5d6b3e04e42822e6a6e6ef81c1712000
issverg <issverg@gmail.com>
2019-07-04 12:30:47 +0300

    Security configuration files added

---
+++ GreetingCardsApp/src/main/java/il/co/sela/configuration/AccountConfiguration.java
@@ -0,0 +1,27 @@
+package il.co.sela.configuration;
+
+import java.util.Base64;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AccountConfiguration {
+	public AccountUserCredentials tokenDecode(String token) {
+		int index = token.indexOf(" ");
+		token = token.substring(index + 1);
+		byte[] base64DecodeBytes = Base64.getDecoder().decode(token);
+		token = new String(base64DecodeBytes);
+		String[] auth = token.split(":");
+		AccountUserCredentials credentials = 
+				new AccountUserCredentials(auth[0], auth[1]);
+		return credentials;
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
