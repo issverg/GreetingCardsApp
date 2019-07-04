464761454c2847efc50c51748b00d3b15fa76bbf
issverg <issverg@gmail.com>
2019-07-04 12:31:08 +0300

    Implementing swagger docs ui

---
+++ GreetingCardsApp/src/main/java/il/co/sela/configuration/SwaggerConfig.java
@@ -0,0 +1,93 @@
+package il.co.sela.configuration;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.common.base.Predicates;
+import com.google.common.collect.Lists;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.DocExpansion;
+import springfox.documentation.swagger.web.ModelRendering;
+import springfox.documentation.swagger.web.OperationsSorter;
+import springfox.documentation.swagger.web.TagsSorter;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.paths(Predicates.not(PathSelectors.regex("/error")))
+				.build()
+				.useDefaultResponseMessages(false)
+				.securitySchemes(Lists.newArrayList(apiKey()))
+				.securityContexts(Lists.newArrayList(securityContext()))
+				.enableUrlTemplating(true)
+				.apiInfo(apiInfo());
+	}
+
+	private ApiKey apiKey() {
+		return new ApiKey("token", "token", "Header");
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+						.title("MyGreetingCardsApp")
+						.description("Excercise app for Sela.co.il.")
+						.license("Created by Vekshinskiy Nikita.")
+						.version("1.0v")
+						.build();
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder()
+				.securityReferences(defaultAuth())
+				.forPaths(PathSelectors.any())
+				.build();
+	}
+
+	List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Lists.newArrayList(new SecurityReference("token", authorizationScopes));
+	}
+
+	@Bean
+	UiConfiguration uiConfig() {
+		return UiConfigurationBuilder.builder()
+				.deepLinking(true)
+				.displayOperationId(false)
+				.defaultModelsExpandDepth(1)
+				.defaultModelExpandDepth(1)
+				.defaultModelRendering(ModelRendering.EXAMPLE)
+				.displayRequestDuration(false)
+				.docExpansion(DocExpansion.NONE)
+				.filter(false)
+				.maxDisplayedTags(null)
+				.operationsSorter(OperationsSorter.ALPHA)
+				.showExtensions(false)
+				.tagsSorter(TagsSorter.ALPHA)
+				.supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+				.validatorUrl(null)
+				.build();
+	}
+}
