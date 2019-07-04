26b96ebbaa561394fed24f679266ab9629813fdf
issverg <issverg@gmail.com>
2019-07-04 12:31:38 +0300

    Dto for login input added

---
+++ GreetingCardsApp/src/main/java/il/co/sela/dto/LoginInDto.java
@@ -0,0 +1,15 @@
+package il.co.sela.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginInDto {
+	String email;
+	String password;
+}
