249bf1351ba8443fe88ae78e530afc72253f9825
issverg <issverg@gmail.com>
2019-07-06 14:04:03 +0300

    Admin creation has removed.

--- GreetingCardsApp/src/main/java/il/co/sela/targil/GreetingCardsAppApplication.java
+++ GreetingCardsApp/src/main/java/il/co/sela/targil/GreetingCardsAppApplication.java
@@ -1,35 +1,12 @@
 package il.co.sela.targil;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import il.co.sela.targil.dao.UsersRepository;
-import il.co.sela.targil.domain.Client;
 
 @SpringBootApplication
-public class GreetingCardsAppApplication implements CommandLineRunner {
-	@Autowired
-	PasswordEncoder encoder;
-	@Autowired
-	UsersRepository repository;
+public class GreetingCardsAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GreetingCardsAppApplication.class, args);
-	}
-	
-	@Override
-	public void run(String... args) throws Exception {
-		String pass = encoder.encode("adminadmin");
-		if (!repository.existsById("iss@gmail.com")) {
-			Client client = Client.builder()
-					.email("iss@gmail.com")
-					.password(pass)
-					.role("ROLE_ADMIN")
-					.build();
-			repository.save(client);
-		}
 	}
 }
