70d97151efc10923cb9ce12a227839aa287de567
issverg <issverg@gmail.com>
2019-07-06 23:46:59 +0300

    Changing params of some methods

--- GreetingCardsApp/src/main/java/il/co/sela/targil/service/cards/ICardsService.java
+++ GreetingCardsApp/src/main/java/il/co/sela/targil/service/cards/ICardsService.java
@@ -1,5 +1,24 @@
 package il.co.sela.targil.service.cards;
 
-public interface ICardsService {
+import java.util.List;
 
+import il.co.sela.targil.dto.CardInputDto;
+import il.co.sela.targil.dto.CardOutputDto;
+import il.co.sela.targil.dto.TemplateInputDto;
+
+public interface ICardsService {
+	
+	List<CardOutputDto> getCatalog();
+	
+	List<CardOutputDto> getMyTemplates();
+	
+	List<CardOutputDto> getMyCards();
+	
+	CardOutputDto createCardByTemplate(CardInputDto cardInputDto, String id);
+	
+	CardOutputDto createNewTemplate(TemplateInputDto templateInputDto);
+	
+	CardOutputDto createNewCard(CardInputDto cardInputDto);
+	
+	boolean removeCard(String id);
 }
