c3c73e6e0c5f5bcbb0443394c4b96ff4531d1f7d
issverg <issverg@gmail.com>
2019-07-07 23:03:29 +0300

    Adding swagger api documentation

--- GreetingCardsApp/src/main/java/il/co/sela/targil/controller/AuthController.java
+++ GreetingCardsApp/src/main/java/il/co/sela/targil/controller/AuthController.java
@@ -9,19 +9,29 @@ import il.co.sela.targil.dto.LoginInputDto;
 import il.co.sela.targil.dto.LoginOutputDto;
 import il.co.sela.targil.dto.RegUserDto;
 import il.co.sela.targil.service.users.IUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
+@Api(description = "REST API Controller for authorization methods.")
 @RestController
 public class AuthController {
 	@Autowired
 	IUserService userService;
 	
+	@ApiOperation("Registration. Public free access method.")
 	@PostMapping("/signup")
-	public LoginOutputDto signUp(@RequestBody RegUserDto regUserDto) {
+	public LoginOutputDto signUp(
+			@ApiParam("Data transfer object for registration.")
+			@RequestBody RegUserDto regUserDto) {
 		return userService.signUp(regUserDto);
 	}
 	
+	@ApiOperation("Logging in. Public free access method.")
 	@PostMapping("/signin")
-	public LoginOutputDto signIn(@RequestBody LoginInputDto loginInputDto) {
+	public LoginOutputDto signIn(
+			@ApiParam("Data transfer object for logging in.")
+			@RequestBody LoginInputDto loginInputDto) {
 		return userService.signIn(loginInputDto);
 	}
 }
--- GreetingCardsApp/src/main/java/il/co/sela/targil/controller/GreetingCardsController.java
+++ GreetingCardsApp/src/main/java/il/co/sela/targil/controller/GreetingCardsController.java
@@ -17,45 +17,66 @@ import il.co.sela.targil.dto.CardInputDto;
 import il.co.sela.targil.dto.CardOutputDto;
 import il.co.sela.targil.dto.TemplateInputDto;
 import il.co.sela.targil.service.cards.ICardsService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
+@Api(description = "REST API Controller for methods interacting with cards.")
 @RestController
 @RequestMapping("/greetingCard")
 public class GreetingCardsController {
 	@Autowired
 	ICardsService cardService;
 	
+	@ApiOperation("Getting list of card templates. Public free access method.")
 	@GetMapping("/catalog")
 	public List<CardOutputDto> getCatalog() {
 		return cardService.getCatalog();
 	}
 	
+	@ApiOperation("Getting list of card templates of the user. Token access method.")
 	@GetMapping("/templates")
 	public List<CardOutputDto> getMyTemplates() {
 		return cardService.getMyTemplates();
 	}
 
+	@ApiOperation("Getting list of cards of the user. Token access method.")
 	@GetMapping("/cards")
 	public List<CardOutputDto> getMyCards() {
 		return cardService.getMyCards();
 	}
 
+	@ApiOperation("Creating card by given template. Token access method.")
 	@PostMapping("/byTemplate/{id}")
-	public CardOutputDto createCardByTemplate(@RequestBody CardFromTemplateInputDto cardFromTemplateInputDto, @PathVariable String id) {
+	public CardOutputDto createCardByTemplate(
+			@ApiParam("Data transfer object with fields filled on the client side according given template.")
+			@RequestBody CardFromTemplateInputDto cardFromTemplateInputDto, 
+			@ApiParam("Id of template.")
+			@PathVariable String id) {
 		return cardService.createCardByTemplate(cardFromTemplateInputDto, id);
 	}
 
+	@ApiOperation("Creating new template. Token access method.")
 	@PostMapping("/newTemplate")
-	public CardOutputDto createNewTemplate(@RequestBody TemplateInputDto templateInputDto) {
+	public CardOutputDto createNewTemplate(
+			@ApiParam("Data transfer object for the template to be created.")
+			@RequestBody TemplateInputDto templateInputDto) {
 		return cardService.createNewTemplate(templateInputDto);
 	}
 
+	@ApiOperation("Creating new card from scratch. Token access method.")
 	@PutMapping("/newCard")
-	public CardOutputDto createNewCard(@RequestBody CardInputDto cardInputDto) {
+	public CardOutputDto createNewCard(
+			@ApiParam("Data transfer object for the card to be created.")
+			@RequestBody CardInputDto cardInputDto) {
 		return cardService.createNewCard(cardInputDto);
 	}
 	
+	@ApiOperation("Removing card by id. Token access method.")
 	@DeleteMapping("remove/{id}")
-	public boolean removeCard(@PathVariable String id) {
+	public boolean removeCard(
+			@ApiParam("Id of the card to be removed.")
+			@PathVariable String id) {
 		return cardService.removeCard(id);
 	}
 }
